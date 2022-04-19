141238cd5746a6f51c5a65e868af4cd17dc09d1c
TheShadowKing19 <mikiwolf19@gmail.com>
2022-04-19 19:14:27 +0200

    Do zadania 5

--- main.py
+++ main.py
@@ -21,15 +21,6 @@ def entropy(x):
     return -suma
 
 
-
-# def entropy(x):
-#     _sum = 0
-#     _, y = freq(x)
-#     for p in y:
-#         _sum -= p * math.log(p, 2)
-#     return _sum
-
-
 def info_gain(x, y):
     suma = 0
     _, _, z = freq2(x, y)
@@ -38,19 +29,43 @@ def info_gain(x, y):
     return entropy(x) + entropy(y) - -suma
 
 
+def convert_to_sparse_pandas(DataFrame, exclude_columns=[]):
+    DataFrame = DataFrame.copy()
+    exclude_columns = set(exclude_columns)
+    for (columnName, columnData) in DataFrame.iteritems():
+        if columnName not in exclude_columns:
+            DataFrame[columnName] = pd.SparseArray(columnData.values, fill_value=0, dtype='uint8')
+
+    return DataFrame
+
+
 if __name__ == '__main__':
     x = pd.read_csv("zoo.csv")
-    print(freq2(x['legs'], x['type'], prob=True))
+    print(freq(x['legs']))
+    print(freq(x['legs'], prob=False))
+    print("\n", freq2(x['legs'], x['type'], prob=True))
+    print("\n", freq2(x['legs'], x['type'], prob=False))
     print(entropy(x['type']))
     print(info_gain(x['domestic'], x['type']))
     print(x.columns)
+    print("\n\n")
+
+    # Zad 4
     x = pd.read_csv("zoo.csv")
     for i in x.columns:
         if i != 'type':
             print(i, info_gain(x[i], x['type']))
-#zad4
-# for i in x.columns:
-#     if i!='type':
-#         #print("działa")
-#         z=info_gain(x[i],x['type'])
-#         print(i,z)
+    print("\n\n")
+
+    # Zad 5
+    data, numbers = freq(x['legs'])
+    data_one_hot = pd.get_dummies(x, columns=x.columns)
+    print(data_one_hot.head(), data_one_hot.dtypes)
+    data_one_hot_sparse = convert_to_sparse_pandas(data_one_hot)
+    print(data_one_hot_sparse.head(), data_one_hot_sparse.dtypes)
+    print("\n\n")
+    print(freq(data_one_hot_sparse['legs_4']))
+    print(freq2(data_one_hot_sparse['eggs_True'], data_one_hot_sparse['type_bird'], prob=True))
+
+    # Zad 6
+
